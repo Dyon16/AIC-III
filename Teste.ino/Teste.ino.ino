3956121e0488d53854fabccc7fff46a5b8b3864b
Jardel <jardel.dyonisio@hotmail.com>
2021-08-17 21:56:04 -0300

    First commit

---
+++ Teste.ino/Teste.ino.ino
@@ -0,0 +1,22 @@
+#include "EmonLib.h" //INCLUSÃO DE BIBLIOTECA
+
+#define CURRENT_CAL 18.40 //VALOR DE CALIBRAÇÃO (DEVE SER AJUSTADO EM PARALELO COM UM MULTÍMETRO MEDINDO A CORRENTE DA CARGA)
+const int pinoSensor = A0; //PINO ANALÓGICO EM QUE O SENSOR ESTÁ CONECTADO
+
+EnergyMonitor emon1; //CRIA UMA INSTÂNCIA
+
+void setup()
+{  
+  Serial.begin(9600); //INICIALIZA A SERIAL
+  emon1.current(pinoSensor, CURRENT_CAL); //PASSA PARA A FUNÇÃO OS PARÂMETROS (PINO ANALÓGIO / VALOR DE CALIBRAÇÃO)
+}
+
+void loop()
+{
+  emon1.calcVI(17, 100); //FUNÇÃO DE CÁLCULO (17 SEMICICLOS / TEMPO LIMITE PARA FAZER A MEDIÇÃO)
+  double currentDraw = emon1.Irms; //VARIÁVEL RECEBE O VALOR DE CORRENTE RMS OBTIDO
+
+    Serial.print("Corrente medida: "); //IMPRIME O TEXTO NA SERIAL
+    Serial.print(currentDraw); //IMPRIME NA SERIAL O VALOR DE CORRENTE MEDIDA
+    Serial.println("A"); //IMPRIME O TEXTO NA SERIAL
+}
